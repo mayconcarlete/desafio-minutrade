6121081b7a1f467dfaf27f8c8cf966ab5835b7f3
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-22 11:11:15 -0300

    test: ensure loadByName return undefined if product doenst exist in DB

--- src/infra/fake-db/products/products.spec.ts
+++ src/infra/fake-db/products/products.spec.ts
@@ -8,6 +8,11 @@ const makeSut = (): SutTypes => {
   return { sut }
 }
 describe('FakeProductsDb', () => {
+  test('Should return undefined if product doenst exists in DB', async () => {
+    const { sut } = makeSut()
+    const result = await sut.loadByName('DOESNT_EXIST')
+    expect(result).toBe(undefined)
+  })
   test('Should return a product if name exists in DB', async () => {
     const { sut } = makeSut()
     const result = await sut.loadByName('CACHORRO')
