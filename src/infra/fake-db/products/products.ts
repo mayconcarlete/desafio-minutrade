ecfab74934ecbd6af9aee41a4df46909284cd23b
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-22 19:20:17 -0300

    feat: ensure FakeProductsDb return an array of products name when loadNames are called

--- src/infra/fake-db/products/products.ts
+++ src/infra/fake-db/products/products.ts
@@ -1,8 +1,12 @@
+import { ILoadProductsByNameAdapter } from '@data/products/protocols/load-all-products-by-name'
 import { TProduct } from '@domain/products/models/products'
 import { ICreateProductAdapter } from 'src/data/products/protocols/create-product'
 import { ILoadProductByNameAdapter } from 'src/data/products/protocols/load-product-by-name'
 
-export class FakeProductsDb implements ILoadProductByNameAdapter, ICreateProductAdapter {
+export class FakeProductsDb implements
+  ILoadProductByNameAdapter,
+  ICreateProductAdapter,
+  ILoadProductsByNameAdapter {
   products: TProduct[]
   constructor () {
     this.products = [
@@ -23,5 +27,10 @@ export class FakeProductsDb implements ILoadProductByNameAdapter, ICreateProduct
   async add (data: TProduct): Promise<TProduct | undefined> {
     this.products.push(data)
     return new Promise(resolve => resolve(data))
+  }
+
+  async loadNames (): Promise<string[] | []> {
+    const arrayNames = this.products.map(product => product.name)
+    return new Promise(resolve => resolve(arrayNames))
   }
 }
