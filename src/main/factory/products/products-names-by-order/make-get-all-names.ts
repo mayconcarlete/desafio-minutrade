a6ffdb0f47338e140e927ac76b6132dfa8a5a7b2
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-22 19:34:29 -0300

    feat: create factory to get all products names sorted by ascending order

---
+++ src/main/factory/products/products-names-by-order/make-get-all-names.ts
@@ -0,0 +1,9 @@
+import { GetAllNames } from '@data/products/usecases/get-all-names/get-all-names'
+import { FakeProductsDb } from '@infra/fake-db/products/products'
+import { GetAllNamesController } from '@presentation/controllers/products/products-names-by-order/get-all-names-by-order'
+
+export const makeGetAllNames = (): GetAllNamesController => {
+  const loadNamesDbAdapter = new FakeProductsDb()
+  const getArrayNames = new GetAllNames(loadNamesDbAdapter)
+  return new GetAllNamesController(getArrayNames)
+}
--- src/main/route/product.ts
+++ src/main/route/product.ts
@@ -1,10 +1,9 @@
+import { makeGetAllNames } from '@main/factory/products/products-names-by-order/make-get-all-names'
 import { Router } from 'express'
 import { adapterRoute } from '../adapter/route-adapter'
 import { makeCreateProduct } from '../factory/products/create-product/make-create-product'
 
 export default (app: Router): void => {
   app.post('/api/v1/products', adapterRoute(makeCreateProduct()))
-  app.get('/api/v1/products', async (req, res) => {
-    return res.json('ok')
-  })
+  app.get('/api/v1/productsnames', adapterRoute(makeGetAllNames()))
 }
