506b7132bb957be8ec4736d379042dc50aecc6e0
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-22 16:54:37 -0300

    chore: add data, main and validations shortcut

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,10 @@ module.exports = {
   },
   moduleNameMapper: {
     '@presentation/(.*)': '<rootDir>/src/presentation/$1',
-    '@domain/(.*)': '<rootDir>/src/domain/$1'
+    '@domain/(.*)': '<rootDir>/src/domain/$1',
+    '@main/(.*)': '<rootDir>/src/main/$1',
+    '@data/(.*)': '<rootDir>/src/data/$1',
+    '@infra/(.*)': '<rootDir>/src/infra/$1',
+    '@validations/(.*)': '<rootDir>/src/validations/$1'
   }
 }
