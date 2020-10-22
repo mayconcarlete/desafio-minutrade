d34560c932548f3075664c67d5e31f142fbc94f2
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-22 10:20:20 -0300

    test: ensure add to have been called with correct params

--- src/data/products/usecases/create-product.spec.ts
+++ src/data/products/usecases/create-product.spec.ts
@@ -1,4 +1,5 @@
 import { TProduct } from '@domain/products/models/products'
+import { ICreateProductAdapter } from '../protocols/create-product'
 import { ILoadProductByNameAdapter } from '../protocols/load-product-by-name'
 import { CreateProduct } from './create-product'
 
@@ -10,7 +11,11 @@ const dataResponse: TProduct = {
   name: 'valid_product',
   price: 123
 }
-
+class MockAddProduct implements ICreateProductAdapter {
+  async add (data: TProduct): Promise<TProduct | undefined> {
+    return new Promise(resolve => resolve(dataResponse))
+  }
+}
 class MockLoadByName implements ILoadProductByNameAdapter {
   async load (name: string): Promise<TProduct | undefined> {
     return new Promise(resolve => resolve(undefined))
@@ -20,12 +25,14 @@ class MockLoadByName implements ILoadProductByNameAdapter {
 type SutTypes = {
   sut: CreateProduct
   mockLoadProductByName: MockLoadByName
+  mockAddProductAdapter: MockAddProduct
 }
 
 const makeSut = (): SutTypes => {
+  const mockAddProductAdapter = new MockAddProduct()
   const mockLoadProductByName = new MockLoadByName()
-  const sut = new CreateProduct(mockLoadProductByName)
-  return { sut, mockLoadProductByName }
+  const sut = new CreateProduct(mockLoadProductByName, mockAddProductAdapter)
+  return { sut, mockLoadProductByName, mockAddProductAdapter }
 }
 
 describe('CreateProduct', () => {
@@ -50,4 +57,19 @@ describe('CreateProduct', () => {
     const result = await sut.create(data)
     expect(result).toBe(undefined)
   })
+  test('Should call add with correct params', async () => {
+    const { sut, mockAddProductAdapter } = makeSut()
+    const mockAddSpy = jest.spyOn(mockAddProductAdapter, 'add')
+    await sut.create(data)
+    expect(mockAddSpy).toHaveBeenCalledWith(data)
+  })
+  // test('Should throw if add throws', async () => {
+  //   const { sut, mockAddProductAdapter } = makeSut()
+  //   jest.spyOn(mockAddProductAdapter, 'add').mock
+  // })
+  // test('should return a new product if they dont exists in DB', async () => {
+  //   const { sut } = makeSut()
+  //   const result = await sut.create(data)
+  //   expect(result).toEqual({ name: 'valid_product', price: 123 })
+  // })
 })
