3b08eb7eced69393b2e3cd1d6cdc90a0d7cb2a93
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-22 18:41:44 -0300

    test: ensure GetAllNames throw if loadNames throw

---
+++ src/data/products/usecases/get-all-names/get-all-names.spec.ts
@@ -0,0 +1,32 @@
+import { ILoadProductsByNameAdapter } from '@data/products/protocols/load-all-products-by-name'
+import { GetAllNames } from './get-all-names'
+
+const products = ['ZEBRA','GATO','GALINHA', 'CACHORRO']
+
+class MockLoadProducts implements ILoadProductsByNameAdapter {
+  async loadNames (): Promise<string[] | []> {
+    return new Promise(resolve => resolve(products))
+  }
+}
+
+type SutTypes = {
+  sut: GetAllNames
+  loadNameProductsAdapter: ILoadProductsByNameAdapter
+}
+
+const makeSut = (): SutTypes => {
+  const loadNameProductsAdapter = new MockLoadProducts()
+  const sut = new GetAllNames(loadNameProductsAdapter)
+  return { sut, loadNameProductsAdapter }
+}
+describe('GetAllNames', () => {
+  test('Should throw if loadNames throw', async () => {
+    const { sut, loadNameProductsAdapter } = makeSut()
+    jest.spyOn(loadNameProductsAdapter, 'loadNames').mockImplementationOnce(async () => {
+      return new Promise(() => {
+        throw new Error()
+      })
+    })
+    await expect(sut.getAllNames()).rejects.toThrow()
+  })
+})
