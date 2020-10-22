403993282dc257382ca43aff9d259bbb71c0919d
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-22 14:46:21 -0300

    feat: implements adapter to comunication between express and controllers

---
+++ src/main/adapter/route-adapter.ts
@@ -0,0 +1,13 @@
+import { THttpRequest } from '@presentation/models'
+import { IController } from '@presentation/protocols'
+import { Request, Response } from 'express'
+
+export const adapterRoute = (controller: IController) => {
+  return async (req: Request, res: Response) => {
+    const request: THttpRequest = {
+      body: req.body
+    }
+    const result = await controller.handle(request)
+    return res.status(result.statusCode).json(result.body)
+  }
+}
