c1f91492b84d6db2de7c60a3f0a44ccf3cb1a054
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-22 17:05:00 -0300

    fix: change wrong word

--- src/validations/errors/min-value-error.ts
+++ src/validations/errors/min-value-error.ts
@@ -1,6 +1,6 @@
 export class MinValueError extends Error {
   constructor (fieldName: string) {
-    super(`${fieldName} field is shorter than minimun`)
+    super(`${fieldName} field is shorter than minimum`)
     this.name = 'MinValueError'
   }
 }
