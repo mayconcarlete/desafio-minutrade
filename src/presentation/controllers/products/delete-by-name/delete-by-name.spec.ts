f822bee3a4ef1a3c2d2803da7574bbcc1d25b6fe
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-23 10:02:10 -0300

    test: ensure DeleteByNameController returns 400 and a error if validation fails

---
+++ src/presentation/controllers/products/delete-by-name/delete-by-name.spec.ts
@@ -0,0 +1,43 @@
+import { TProduct } from '@domain/products/models/products'
+import { IDeleteProduct } from '@domain/products/usecases/delete-product'
+import { THttpRequest } from '@presentation/models'
+import { IValidator } from '@presentation/protocols'
+import { DeleteByNameController } from './delete-by-name'
+
+class MockValidator implements IValidator {
+  validate (input: any): Error | undefined {
+    return undefined
+  }
+}
+class MockDeleteByName implements IDeleteProduct {
+  async deleteProduct (name: string): Promise<TProduct | undefined> {
+    return new Promise(resolve => resolve({ name: 'DELETED_PRODUCT',price: 99 }))
+  }
+}
+type SutTypes = {
+  mockValidator: MockValidator
+  mockDeleteByName: MockDeleteByName
+  sut: DeleteByNameController
+}
+
+const makeSut = (): SutTypes => {
+  const mockValidator = new MockValidator()
+  const mockDeleteByName = new MockDeleteByName()
+  const sut = new DeleteByNameController(mockValidator, mockDeleteByName)
+  return { sut, mockValidator, mockDeleteByName }
+}
+
+describe('DeleteByNameController', () => {
+  test('Should return 400 if validation fails', async () => {
+    const { sut, mockValidator } = makeSut()
+    jest.spyOn(mockValidator, 'validate').mockReturnValueOnce(new Error('Some Validation Fails'))
+    const req: THttpRequest = {
+      body: {
+        name: 'invalid_name'
+      }
+    }
+    const result = await sut.handle(req)
+    expect(result.statusCode).toBe(400)
+    expect(result.body).toEqual('Some Validation Fails')
+  })
+})
