95981606fe322318ae9baec02b15182d951ea50c
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-23 20:16:38 -0300

    test: ensure CreateMultipleProductsController return 400 and a error when validation fails

---
+++ src/presentation/controllers/products/create-multiple-products/create-multiple-products.spec.ts
@@ -0,0 +1,44 @@
+import { TProduct } from '@domain/products/models/products'
+import { ICreateMultipleProducts } from '@domain/products/usecases/create-multiple-products'
+import { InvalidParamError } from '@presentation/errors/invalid-param-error'
+import { THttpRequest } from '@presentation/models'
+import { IValidator } from '@presentation/protocols'
+import { CreateMultipleProductsController } from './create-multiple-products'
+
+class MockValidator implements IValidator {
+  validate (input: any): Error | undefined {
+    return undefined
+  }
+}
+
+const req: THttpRequest = {
+  body: [{ name: 'any_name', price: 88 }]
+}
+const result: TProduct[] = [{ name: 'CAMELO',price: 66 },{ name: 'GIRAFA',price: 66 },{ name: 'ELEFANTE',price: 66 }]
+
+class MockCreateMultipleProducts implements ICreateMultipleProducts {
+  async createMultiples (data: TProduct[]): Promise<TProduct[] | []> {
+    return new Promise(resolve => resolve(result))
+  }
+}
+type SutTypes = {
+  validator: IValidator
+  createMultipleProducts: ICreateMultipleProducts
+  sut: CreateMultipleProductsController
+}
+
+const makeSut = (): SutTypes => {
+  const validator = new MockValidator()
+  const createMultipleProducts = new MockCreateMultipleProducts()
+  const sut = new CreateMultipleProductsController(validator, createMultipleProducts)
+  return { sut, validator,createMultipleProducts }
+}
+describe('CreateMultipleProductsController', () => {
+  test('Shoudl return 400 if validation fails', async () => {
+    const { sut, validator } = makeSut()
+    jest.spyOn(validator, 'validate').mockReturnValueOnce(new InvalidParamError('Validator Fails'))
+    const result = await sut.handle(req)
+    expect(result.statusCode).toBe(400)
+    expect(result.body).toEqual(new InvalidParamError('Validator Fails').message)
+  })
+})
