85c5595bd9ce8b30b2a066dd838163a0f08a6304
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-23 20:15:53 -0300

    feat: ensure CreateMultipleProductsController return 400 and a error when validation fails

---
+++ src/presentation/controllers/products/create-multiple-products/create-multiple-products.ts
@@ -0,0 +1,26 @@
+import { ICreateMultipleProducts } from '@domain/products/usecases/create-multiple-products'
+import { THttpRequest, THttpResponse } from '@presentation/models'
+import { IController, IValidator } from '@presentation/protocols'
+import { badRequest, ok, serverError } from '@presentation/utils/http-responses'
+
+export class CreateMultipleProductsController implements IController {
+  private readonly validators: IValidator
+  private readonly createMultipleProducts: ICreateMultipleProducts
+  constructor (validators: IValidator, createMultipleProducts: ICreateMultipleProducts) {
+    this.validators = validators
+    this.createMultipleProducts = createMultipleProducts
+  }
+
+  async handle (req: THttpRequest): Promise<THttpResponse> {
+    try {
+      const error = this.validators.validate(req.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const newProducts = await this.createMultipleProducts.createMultiples(req.body)
+      return ok(newProducts)
+    } catch (e) {
+      return serverError(e)
+    }
+  }
+}
