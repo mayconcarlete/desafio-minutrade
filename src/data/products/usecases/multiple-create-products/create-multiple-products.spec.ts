841e979cd3fa8f2400518a9fc3699af364d94c7d
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-23 16:06:53 -0300

    refactor: remove unused variables

--- src/data/products/usecases/multiple-create-products/create-multiple-products.spec.ts
+++ src/data/products/usecases/multiple-create-products/create-multiple-products.spec.ts
@@ -19,7 +19,13 @@ describe('CreateMultipleProducts', () => {
   test('Should remove initial and final spaces of names', async () => {
     const { sut } = makeSut()
     const result = await sut.createMultiples(data)
-    expect(result[0].name).toEqual('any_name')
-    expect(result[1].name).toEqual('any_name')
+    expect(result[0].name).toEqual('ANY_NAME')
+    expect(result[1].name).toEqual('ANY_NAME')
+  })
+  test('Should pass all names to uppercase', async () => {
+    const { sut } = makeSut()
+    const result = await sut.createMultiples(data)
+    expect(result[0].name).toEqual('ANY_NAME')
+    expect(result[0].name).toEqual('ANY_NAME')
   })
 })
