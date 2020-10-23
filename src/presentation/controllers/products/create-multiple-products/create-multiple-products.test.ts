6fb93c99e00a47beeddfdb67902b1a18e6908b21
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-23 20:39:04 -0300

    test: ensure integration CreateMultipleProducts return 400 when validation fails

---
+++ src/presentation/controllers/products/create-multiple-products/create-multiple-products.test.ts
@@ -0,0 +1,13 @@
+import supertest from 'supertest'
+import app from '@main/config/app'
+// import { TProduct } from '@domain/products/models/products'
+import { InvalidParamError } from '@presentation/errors/invalid-param-error'
+// const body: TProduct[] = [{ name: 'CAMELO',price: 66 },{ name: 'GIRAFA',price: 66 },{ name: 'ELEFANTE',price: 66 }]
+const errorBody: any[] = []
+describe('POST - multiple-products - CreateMultipleProductsController', () => {
+  test('Should return 400 when validation fails', async () => {
+    const result = await supertest(app).post('/api/v1/multiple-products').send(errorBody)
+    expect(result.status).toBe(400)
+    expect(result.body).toEqual(new InvalidParamError('Invalid array size').message)
+  })
+})
